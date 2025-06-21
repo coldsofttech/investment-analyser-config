caabaee9a087e6aec57769c0ee25495e7667495b
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-06-21 15:41:19 +0100

    Remove create_screener

--- scraper/export_stocks.py
+++ scraper/export_stocks.py
@@ -21,16 +21,6 @@ def accept_all(driver):
     print(f"✅ Accept All button clicked.")
 
 
-def create_screener(driver):
-    wait = get_waiter(driver, 10)
-    print(f"⏳ Awaiting for 'Create' button")
-    create_button = wait.until(EC.element_to_be_clickable(
-        (By.XPATH, '//*[@id="nimbus-app"]/section/section/section/article/section/div[1]/div[1]/button')
-    ))
-    create_button.click()
-    print(f"✅ Create button clicked.")
-
-
 def export_tickers(
         country,
         ticker_type="EQUITY",
@@ -49,7 +39,6 @@ def export_tickers(
 
     driver.get(url)
     accept_all(driver)
-    # create_screener(driver)
     input()
 
 
