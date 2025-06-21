146c3be2648571f22910a83be17fa4bcbcda9d38
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-06-21 16:08:49 +0100

    Create select_country

--- scraper/export_stocks.py
+++ scraper/export_stocks.py
@@ -2,6 +2,7 @@ import argparse
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,6 +20,43 @@ def accept_all(driver):
     ))
     accept_all_button.click()
     print(f"✅ Accept All button clicked.")
+
+
+def select_country(driver, country):
+    wait = get_waiter(driver, 10)
+    print(f"⏳ Awaiting for 'Region' dropdown")
+    region_button = wait.until(EC.element_to_be_clickable(
+        (By.XPATH, '//button[contains(@class, "menuBtn") and .//div[text()="Region"]]')
+    ))
+    region_button.click()
+    print(f"✅ Region dropdown clicked.")
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH, '//div[contains(@class, "dialog-container")]')
+    ))
+    print(f"⏳ Awaiting for 'Reset' button")
+    reset_button = wait.until(EC.element_to_be_clickable(
+        (By.XPATH, '//div[contains(@class, "reset")]//button')
+    ))
+    reset_button.click()
+    print(f"✅ Reset button clicked.")
+    search_text = wait.until(EC.presence_of_element_located(
+        (By.XPATH, '//div[contains(@class, "search")]//input[@placeholder="Search..."]')
+    ))
+    search_text.send_keys(country)
+    search_text.send_keys(Keys.ENTER)
+    country_checkbox = wait.until(EC.presence_of_element_located(
+        (By.XPATH, f'//div[contains(@class, "options")]//label[contains(@class, "input") and @title="{country}"]//input[@type="checkbox"]')
+    ))
+    if not country_checkbox.is_selected():
+        country_checkbox.click()
+        print(f"✅ {country} option selected.")
+    else:
+        print(f"✅ {country} option already selected.")
+    apply_button = wait.until(EC.element_to_be_clickable(
+        (By.XPATH, '//div[contains(@class, "submit")]//button')
+    ))
+    apply_button.click()
+    print(f"✅ Apply button clicked.")
 
 
 def export_tickers(
@@ -39,6 +77,7 @@ def export_tickers(
 
     driver.get(url)
     accept_all(driver)
+    select_country(driver, country)
     input()
 
 
